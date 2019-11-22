ebf3ec16c736fe20a301837ba937efdbbdc01d7f
star0126 <li1324004498@163.com>
2019-11-22 10:20:04 +0800

    李国庆 2019-11-22 10:20

--- src/main/resources/static/js/myjs/home.js
+++ src/main/resources/static/js/myjs/home.js
@@ -11,8 +11,17 @@ $(".title-list ul").on("click", "li", function () {
 $(".duty").find("tbody").find("tr:even").css("backgroundColor", "#eff6fa");
 
 function rili() {
-    /*location.href="/sys/rili.html"*/
-    $("#saveModal").modal('show')
+        layer.open({
+            type: 2,
+            title: '日历',
+            shadeClose: false,
+            shade: 0.5,
+            skin: 'layui-layer-rim',
+            closeBtn: 2,
+            area: ['450px', '350px'],
+            content: 'rili.html'
+
+        });
 }
 
 function day() {
